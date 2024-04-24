b1c98a854a5ae16e250996eaedb3614a6a0bc0fd
JamalArsh <92667028+JamalArsh@users.noreply.github.com>
2024-04-24 08:55:46 +0530

    files created

---
+++ src/controllers/ToDoController.js
@@ -0,0 +1,32 @@
+const ToDoModel = require("../models/ToDoModel");
+
+module.exports.getToDo = async (req, res) => {
+  const toDo = await ToDoModel.find();
+  res.send(toDo);
+};
+
+module.exports.saveToDo = async (req, res) => {
+  const { title, description, isCompleted } = req.body;
+
+  ToDoModel.create({ title, description, isCompleted }).then((data) => {
+    console.log("Added succesfully");
+    console.log(data);
+    res.send(data);
+  });
+};
+
+module.exports.updateToDo = async (req, res) => {
+  const { _id, title, description, isCompleted } = req.body;
+
+  ToDoModel.findByIdAndUpdate(_id, { title, description, isCompleted })
+    .then(() => res.send("Updated Successfully"))
+    .catch((err) => console.log(err));
+};
+
+module.exports.deleteToDo = async (req, res) => {
+  const { _id } = req.body;
+
+  ToDoModel.findByIdAndDelete(_id)
+    .then(() => res.send("Deleted Successfully"))
+    .catch((err) => console.log(err));
+};
